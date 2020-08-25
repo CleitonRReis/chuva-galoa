48a4e8244bd2e51c36681ffc66ac53e9be0dc8ab
Cleiton Reis <cleiton-rodrigues30@live.com>
2020-08-25 13:08:56 -0300

    Criado a pasta javaScript e o arquivo script.js

---
+++ javaScript/script.js
@@ -0,0 +1,46 @@
+
+function verMais() {
+
+    let verMais = document.getElementById('ver-mais');
+    let mais = document.getElementById('mais');
+    let paragrafo = document.getElementsByClassName('resumo')[1];
+
+    if (mais.style.display === 'none') {
+        mais.style.display = 'inline';
+        verMais.style.display = 'none';
+        paragrafo.style.height = '530px';
+    } else {
+        mais.style.display = 'none';
+        verMais.style.display = 'inline';
+        paragrafo.style.height = '140px';
+    };
+};
+
+// verMais();
+
+function seeMore() {
+
+    let more = document.getElementById('more');
+    let discussao = document.getElementById('discussao');
+    let pergunta = document.getElementsByClassName('pergunta')[1];
+    let ideias = document.getElementsByClassName('ideias')[0];
+
+    let like = document.getElementById('like');
+    let res = document.getElementById('res');
+
+    if (more.style.display === 'none'){
+        more.style.display = 'inline';
+        discussao.style.display = 'none';
+        pergunta.style.height = '750px';
+        ideias.style.height = '1230px'
+        like.innerHTML = '4 likes'
+        res.innerHTML = '4 respostas'
+    } else {
+        more.style.display = 'none';
+        discussao.style.display = 'inline';
+        pergunta.style.height = '145px';
+        ideias.style.height = '620px'
+        like.innerHTML = '1 like'
+        res.innerHTML = '1 resposta'
+    }
+}
